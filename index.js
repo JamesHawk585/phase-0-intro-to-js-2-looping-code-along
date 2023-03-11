074ec9cd62f1aa815662336b4f1165eeabd26906
James Hawk <jph94880@gmail.com>
2023-03-11 16:56:40 -0600

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const friendNames = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, events) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num --;
+    }
+}
+ 
